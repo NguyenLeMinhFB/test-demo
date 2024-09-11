c462a44c178cd438442ea6196c0765017e0c46d8
minh-dev <nguyenleminh@fabercompany.co.jp>
2024-09-11 09:40:56 +0700

    commit code

--- optimize-handler/src/main/java/org/example/Main.java
+++ optimize-handler/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test gitHub action 1!");
+        System.out.println("Test gitHub action 3!");
     }
 }
